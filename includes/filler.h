b0ef3363a493ef18b39150f4c21b361967b052c0
Caruy Chen <caruychen@gmail.com>
2022-05-04 11:58:25 +0300

    began preliminary input parsing

--- includes/filler.h
+++ includes/filler.h
@@ -6,13 +6,24 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 10:01:17 by cchen             #+#    #+#             */
-/*   Updated: 2022/05/04 10:01:47 by cchen            ###   ########.fr       */
+/*   Updated: 2022/05/04 11:51:22 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLER_H
 # define FILLER_H
 
+# define ERROR 0
+/* DELETE ME */
+# include <stdio.h>
+/* END DELETE ME */
+
 # include "libft.h"
+
+typedef struct s_map
+{
+	char	me;
+	char	enemy;
+}				t_map;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,41 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 10:00:00 by cchen             #+#    #+#             */
-/*   Updated: 2022/05/04 10:00:54 by cchen            ###   ########.fr       */
+/*   Updated: 2022/05/04 11:57:38 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
+void init_players(t_map *map)
+{
+	char	*line;
+	char	*p_num;
+
+	if (map->me)
+		return ;
+	if (get_next_line(0, &line) <= 0)
+		return ;
+	p_num = ft_strchr(line, 'p');
+	if (!p_num++)
+		return ;
+	if (!(*p_num == '1' || *p_num == '2'))
+		return ;
+	map->me = 'O' + (*p_num == '2') * 9; 
+	map->enemy = 'X' - (*p_num == '2') * 9;
+}
+
+void	init(t_map *map)
+{
+	ft_bzero(map, sizeof(*map));
+	init_players(map);
+}
+
 int	main(void)
 {
-	ft_printf("hello\n");
+	t_map map;
+	init(&map);
+	dprintf(2, "Testing: %c %c\n", map.me, map.enemy);
+	ft_printf("0 0");
 	return (0);
 }
