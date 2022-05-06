b3ea1e4b84203d0b25cbef124995661012466b2b
Caruychen <caruychen@gmail.com>
2022-05-06 16:42:09 +0300

    free line on error

--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_size.c                                        :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 13:50:14 by cchen             #+#    #+#             */
-/*   Updated: 2022/05/06 15:12:02 by cchen            ###   ########.fr       */
+/*   Updated: 2022/05/06 16:40:19 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ int	get_dimensions(t_dimension *dimension, char *name)
 	if (get_next_line(STDIN, &line) <= 0)
 		return (error(NULL, "Memory allocation error: get_dimensions()"));
 	if (!ft_strstr(line, name))
+	{
+		ft_strdel(&line);
 		return (error(NULL, "Error reading dimension: get_dimensions()"));
+	}
 	dimension->h = ft_atoi(ft_strchr(line, ' '));
 	dimension->w = ft_atoi(ft_strrchr(line, ' '));
 	ft_strdel(&line);
