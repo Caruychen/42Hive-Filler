9edefdf52d68d94ecd1c9369c4007dc2ef79063d
Caruychen <caruychen@gmail.com>
2022-05-08 11:00:25 +0300

    added read_board comment

--- src/read_board.c
+++ src/read_board.c
@@ -1,14 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_map.c                                         :+:      :+:    :+:   */
+/*   read_board.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 13:10:11 by cchen             #+#    #+#             */
-/*   Updated: 2022/05/08 10:06:34 by cchen            ###   ########.fr       */
+/*   Updated: 2022/05/08 11:00:07 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+* Attempts to Make a new empty board and fills the board using set_grid, which
+* reads input line by line into the declared grid.
+* The line is skipped twice. First, if the board grid already exists (mid-game)
+* then we skip the dimensions line.
+* The second skip skips the number line 01234....
+*/
 
 #include "filler.h"
 
