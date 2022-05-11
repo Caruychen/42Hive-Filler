47e2781d9e1d0efd4a061b56dca044292abc77e2
Caruy Chen <caruychen@gmail.com>
2022-05-11 16:02:38 +0300

    mod test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 make
 #./resources/filler_vm -f ./resources/maps/new00 -p1 ./cchen.filler -p2 ./resources/players/superjeannot.filler | grep "=="
 #./resources/filler_vm -f ./resources/maps/map00 -p1 ./cchen.filler -p2 ./resources/players/hcao.filler
-./resources/filler_vm -f ./resources/maps/map01 -p2 ./cchen.filler -p1 ./resources/players/hcao.filler
+./resources/filler_vm -f ./resources/maps/map01 -p1 ./cchen.filler -p2 ./resources/players/grati.filler
 #./resources/filler_vm -f ./resources/maps/map02 -p1 ./cchen.filler -p2 ./resources/players/hcao.filler
