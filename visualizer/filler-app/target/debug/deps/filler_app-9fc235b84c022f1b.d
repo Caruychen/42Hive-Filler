1e5bb679a96b569cd15a38bb931d9a51388a6499
Caruychen <caruychen@gmail.com>
2022-05-12 19:36:29 +0300

    initialized basic rust server

---
+++ visualizer/filler-app/target/debug/deps/filler_app-9fc235b84c022f1b.d
@@ -0,0 +1,5 @@
+/Users/caruychen/Hive/42Hive-Filler/visualizer/filler-app/target/debug/deps/filler_app-9fc235b84c022f1b: src/main.rs
+
+/Users/caruychen/Hive/42Hive-Filler/visualizer/filler-app/target/debug/deps/filler_app-9fc235b84c022f1b.d: src/main.rs
+
+src/main.rs:
---
+++ visualizer/filler-app/target/debug/filler-app.d
@@ -0,0 +1,1 @@
+/Users/caruychen/Hive/42Hive-Filler/visualizer/filler-app/target/debug/filler-app: /Users/caruychen/Hive/42Hive-Filler/visualizer/filler-app/src/main.rs
