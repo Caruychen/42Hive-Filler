c9a298fb4b326a04d3ef0245642c87f22aaa7b92
Caruychen <caruychen@gmail.com>
2022-05-19 09:53:46 +0300

    breakout dispatch functions

--- visualizer/filler-app/src/bin/main.rs
+++ visualizer/filler-app/src/bin/main.rs
@@ -22,26 +22,7 @@ fn handle_connection(mut stream: TcpStream) {
 	let mut buffer = [0; 1024];
 	stream.read(&mut buffer).unwrap();
 
-	let get = b"GET / HTTP/1.1\r\n";
-	let getjs = b"GET /js/index.js HTTP/1.1\r\n";
-	let getcss = b"GET /filler.css HTTP/1.1\r\n";
-
-	let (status_line, filename) = if buffer.starts_with(get) {
-		("HTTP/1.1 200 OK", "index.html")
-	} else if buffer.starts_with(getcss) {
-		("HTTP/1.1 200 OK", "filler.css")
-	} else if buffer.starts_with(getjs) {
-/*
-        Filler::run("./assets/filler_vm",
-                    &mut ["-f", "assets/map02",
-                        "-p1", "./assets/players/cchen.filler",
-                        "-p2", "./assets/players/cchen.filler"]);
-*/
-		("HTTP/1.1 200 OK", "js/index.js")	
-	} else {
-		("HTTP/1.1 404 NOT FOUND", "404.html")
-	};
-
+	let (status_line, filename) = dispatch(buffer);
 	let contents = fs::read_to_string(filename).unwrap();
 	
 	let response = format!(
@@ -52,4 +33,28 @@ fn handle_connection(mut stream: TcpStream) {
 	);
 	stream.write(response.as_bytes()).unwrap();
 	stream.flush().unwrap();
+}
+
+fn dispatch(buffer: [u8; 1024]) -> (&'static str, &'static str) {
+	let get = b"GET / HTTP/1.1\r\n";
+	let getjs = b"GET /js/index.js HTTP/1.1\r\n";
+	let getcss = b"GET /filler.css HTTP/1.1\r\n";
+//	let run = b"GET /run HTTP/1.1\r\n";
+
+	let (status_line, filename) = if buffer.starts_with(get) {
+		("HTTP/1.1 200 OK", "index.html")
+	} else if buffer.starts_with(getcss) {
+		("HTTP/1.1 200 OK", "filler.css")
+	} else if buffer.starts_with(getjs) {
+		("HTTP/1.1 200 OK", "js/index.js")	
+/*	} else if buffer.starts_with(run) {
+		Filler::run("./assets/filler_vm",
+			    &mut ["-f", "assets/map02",
+				"-p1", "./assets/players/cchen.filler",
+				"-p2", "./assets/players/cchen.filler"]);
+*/
+	} else {
+		("HTTP/1.1 404 NOT FOUND", "404.html")
+	};
+	(status_line, filename)
 }
